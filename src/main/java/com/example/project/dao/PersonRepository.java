7e48a1a459a3f1bf2b453a6ea231661a50fcb916
Jarek <jkormanski@gmail.com>
2017-03-18 20:00:10 +0100

    spring boot data rest example
    spring boot data rest example

---
+++ src/main/java/com/example/project/dao/PersonRepository.java
@@ -0,0 +1,16 @@
+package com.example.project.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.example.project.entity.Person;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+	List<Person> findByLastname(@Param("name") String name);
+
+}
